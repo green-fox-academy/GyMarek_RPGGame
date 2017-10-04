cf3bdc14f3f15142cfef423e39eda33b67a50d1f
Gyula Nemes <nemes.gyula.p@gmail.com>
2017-10-04 16:16:55 +0200

    Hero move done, Skeleton random done

---
+++ RPGGame/RPGGame/Boss.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreenFox;
+
+namespace RPGGame
+{
+    public class Boss : Character
+    {
+        public static FoxDraw foxDraw;
+        Random randomBoss = new Random();
+        public static int startingHeroX = 50;
+        public static int startingHeroY = 50;
+
+        public void Drawboss(FoxDraw foxDraw)
+        {
+            Boss.foxDraw = foxDraw;
+            
+                int x = randomBoss.Next(1, 11);
+                int y = randomBoss.Next(1, 11);
+                if (skeletonMatrix[y][x] == true)
+                {
+                foxDraw.AddImage("./Assets/boss.png", startingHeroX * x, startingHeroY * y);   
+                    skeletonMatrix[y][x] = false;
+                }           
+        }
+
+    }
+}
--- RPGGame/RPGGame/Skeleton.cs
+++ RPGGame/RPGGame/Skeleton.cs
@@ -14,15 +14,40 @@ namespace RPGGame
         Random randomSkeleton = new Random();
         public static int startingHeroX = 50;
         public static int startingHeroY = 50;
-        private static int boardsize = 50;
-        private Canvas Canvas { get; set; }
+        //private static int boardsize = 50;
+        //private Canvas Canvas { get; set; }
+
+        private static List<bool[]> skeletonMatrix = new List<bool[]>
+        {
+            new bool[] { false,false,false,false,false,false,false,false,false,false,false,false },
+            new bool[] { false, false, true, true, false, true, false, true, true, true, true, false },
+            new bool[] { false, true, true, true, false, true, false, true, false, false, true, false },
+            new bool[] { false, true, false, false, false, true, false, true, false, false, true, false },
+            new bool[] { false, true, true, true, true, true, false, true, true, true, true, false },
+            new bool[] { false, false, false, false, false, true, false, false, false, false, true, false},
+            new bool[] { false, true, false, true, false, true, true, true, true, false, true, false },
+            new bool[] { false, true, false, true, false, true, false, false, true, false, true, false },
+            new bool[] { false, true, true, true, true, true, false, false, true, true, true, false },
+            new bool[] { false, true, false, false, false, true, true, true, true, false, true, false },
+            new bool[] { false, true, true, true, false, true, false, false, true, true, true, false },
+            new bool[] { false,false,false,false,false,false,false,false,false,false,false,false }
+        };
 
         public void Drawskeleton(FoxDraw foxDraw)
         {
             Skeleton.foxDraw = foxDraw;
-            foxDraw.AddImage("./Assets/skeleton.png", startingHeroX * randomSkeleton.Next(1, 10), startingHeroY * randomSkeleton.Next(1, 10));
-            foxDraw.AddImage("./Assets/skeleton.png", startingHeroX * randomSkeleton.Next(1, 10), startingHeroY * randomSkeleton.Next(1, 10));
-            foxDraw.AddImage("./Assets/skeleton.png", startingHeroX * randomSkeleton.Next(1, 10), startingHeroY * randomSkeleton.Next(1, 10));
+            int noOfSkeleton = 0;
+            do
+            {
+                int x = randomSkeleton.Next(1, 11);
+                int y = randomSkeleton.Next(1, 11);
+                if (skeletonMatrix[y][x]==true)
+                {
+                    foxDraw.AddImage("./Assets/skeleton.png", startingHeroX * x, startingHeroY * y);
+                    noOfSkeleton++;
+                    skeletonMatrix[y][x] = false;
+                }                
+            } while (noOfSkeleton < 3);
         }
     }
 }
